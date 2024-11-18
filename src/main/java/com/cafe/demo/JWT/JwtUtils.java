b2a6d699fb433ffd50327f252903d47635dc45bb
Li <aaa111334@gmail.com>
2024-11-18 21:26:54 +0800

    modified JwtUtils a little

--- src/main/java/com/cafe/demo/JWT/JwtUtils.java
+++ src/main/java/com/cafe/demo/JWT/JwtUtils.java
@@ -1,8 +1,24 @@
 package com.cafe.demo.JWT;
 
+import java.util.function.Function;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 
 @Service
 public class JwtUtils {
-    
+
+    private String secreat = "demo11";
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secreat).parseClaimsJws(token).getBody();
+    }
+
 }
